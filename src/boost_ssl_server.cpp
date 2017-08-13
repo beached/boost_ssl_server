0bbd9ac22b75f5578a206a119ac91a8b8bac30ee
Darrell Wright <dwright-git@dawdevel.ca>
2017-08-13 19:14:23 -0400

    small cleanup

--- src/boost_ssl_server.cpp
+++ src/boost_ssl_server.cpp
@@ -25,7 +25,7 @@ class session {
 
   public:
 	session( boost::asio::io_service &io_service, boost::asio::ssl::context &context )
-	    : m_socket{io_service, context} {}
+	    : m_socket{io_service, context}, m_data{0} {}
 
 	ssl_socket::lowest_layer_type &socket( ) {
 		return m_socket.lowest_layer( );
@@ -88,7 +88,7 @@ class server {
 		m_context.use_private_key_file( "server.pem", boost::asio::ssl::context::pem );
 		m_context.use_tmp_dh_file( "dh512.pem" );
 
-		session *new_session = new session( m_io_service, m_context );
+		auto new_session = new session( m_io_service, m_context );
 		m_acceptor.async_accept( new_session->socket( ), [this, new_session]( boost::system::error_code const &error ) {
 			handle_accept( new_session, error );
 		} );
@@ -120,8 +120,7 @@ int main( int argc, char *argv[] ) {
 
 		boost::asio::io_service io_service;
 
-		using namespace std; // For atoi.
-		server s( io_service, static_cast<uint16_t>( atoi( argv[1] ) ) );
+		server s( io_service, static_cast<uint16_t>( strtol( argv[1], nullptr, 10 ) ) );
 
 		io_service.run( );
 	} catch( std::exception &e ) {
